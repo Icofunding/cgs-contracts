7b51de012fe3e7c63f9768920e4275857af4427a
Adrián Calvo <adrianclv@gmail.com>
2018-01-12 13:19:07 +0100

    empty project

---
+++ truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      gas: 6712390,
+      network_id: "*" // Match any network id
+    }
+  }
+};
