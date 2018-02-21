d72b1064855bd8736fea75abac1c954686859453
Adrián Calvo <adrianclv@gmail.com>
2018-02-21 19:26:12 +0100

    add metadata to testToken

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -9,23 +9,29 @@ module.exports = async function(deployer, network, accounts) {
   const NOW = Math.floor(Date.now() / 1000);
 
   // Fake CGS Token
-  let icoInitialSupply = 1000;
+  let icoInitialSupply = "10000000000000000000000";
   let tokenHolder = accounts[0]; // Write your ethereum address here
+  let cgsTokenName = "Coin Governance System";
+  let cgsTokenSymbol = "CGS";
+  let cgsTokenDecimals = 18;
 
   // Fake ICO Token
-  let cgsInitialSupply = 1000;
+  let cgsInitialSupply = "10000000000000000000000";
   let cgsHolder = accounts[0]; // Write your ethereum address here
+  let icoTokenName = "ICO Token";
+  let icoTokenSymbol = "ICOT";
+  let icoTokenDecimals = 18;
 
   // CGSFactory
   let weiPerSecond = 5;
   let claimPrice = 500;
   let icoLauncher = accounts[0]; // Write your ethereum address here
 
-  await deployer.deploy(ICOTestToken, tokenHolder, icoInitialSupply);
+  await deployer.deploy(ICOTestToken, tokenHolder, icoInitialSupply, icoTokenName, icoTokenSymbol, icoTokenDecimals);
   await deployer.deploy(CGSBinaryVote, CGSTestToken.address);
   await deployer.deploy(CGSFactory, CGSBinaryVote.address);
 
-  await deployer.deploy(CGSTestToken, cgsHolder, cgsInitialSupply);
+  await deployer.deploy(CGSTestToken, cgsHolder, cgsInitialSupply, cgsTokenName, cgsTokenSymbol, cgsTokenDecimals);
 
   let CGSFactoryContract = await CGSFactory.deployed();
   let event = (await CGSFactoryContract.create(weiPerSecond, claimPrice, icoLauncher, ICOTestToken.address, NOW, {from: icoLauncher})).logs[0];
