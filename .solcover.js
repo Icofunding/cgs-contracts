d9605b295c097a2bbd4c1c879895bbb1a9f5c230
Adrián Calvo <adrianclv@gmail.com>
2018-09-25 18:38:03 +0200

    coverage

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 6545,
+    testrpcOptions: '-p 6545 -e 1000 -m "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat"',
+    skipFiles: ['test', 'util']
+};
